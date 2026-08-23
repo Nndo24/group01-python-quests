e30042744bc99d122ba784302e44dee6b67dbb94
Nndo24 <n.ukoh@alustudent.com>
2026-08-23 11:15:02 +0400

    Update quest_14_logical_gatekeeper.py

---
+++ quests/quest_14_logical_gatekeeper.py
@@ -0,0 +1,7 @@
+age = int(input("What is your age? "))
+gold = int(input("How many gold coins do you possess? "))
+
+if age >= 18 and gold >= 20:
+    print("Welcome to the Secret Society Guild hall!")
+else:
+    print("Begone! You do not meet the age or wealth requirements.")
