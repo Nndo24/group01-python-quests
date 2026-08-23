3ff6f4fd0ac1a83a1de3f1218caadc62ab922feb
Nndo24 <n.ukoh@alustudent.com>
2026-08-23 11:11:40 +0400

    Update quest_23_oracles_vision.py

---
+++ quests/quest_23_oracles_vision.py
@@ -0,0 +1,8 @@
+def calculate_area(length, width):
+    return length * width
+
+room_1 = calculate_area(10.5, 8.0)
+room_2 = calculate_area(15.0, 12.0)
+
+print(f"Area of the First Chamber: {room_1} sq meters")
+print(f"Area of the Main Hall: {room_2} sq meters")
