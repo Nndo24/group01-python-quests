fdd4573a029242994f85223be2acd38846bdf92d
Nndo24 <n.ukoh@alustudent.com>
2026-08-23 11:12:03 +0400

    Update quest_22_personalized_scroll.py

---
+++ quests/quest_22_personalized_scroll.py
@@ -0,0 +1,7 @@
+def personalized_greeting(name, quest):
+    print(f"Hail, {name}! May your quest to '{quest}' bring glory to the kingdom.")
+
+user_name = input("Enter your hero name: ")
+user_quest = input("Enter your quest objective: ")
+
+personalized_greeting(user_name, user_quest)
