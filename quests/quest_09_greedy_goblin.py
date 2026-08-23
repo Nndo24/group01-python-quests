cedfa7fa9b68ef4e6341595fbeb4c6816beacc04
Nndo24 <n.ukoh@alustudent.com>
2026-08-23 10:33:29 +0400

    Update quest_09_greedy_goblin.py

---
+++ quests/quest_09_greedy_goblin.py
@@ -0,0 +1,8 @@
+total_gold = 27
+friends_count = 4
+
+share_per_friend = total_gold // friends_count
+goblin_remainder = total_gold % friends_count
+
+print(f"Each friend receives {share_per_friend} gold pieces.")
+print(f"The greedy goblin keeps the remaining {goblin_remainder} gold piece(s) for himself!")
